9d414bf6764b1a25eda10e5cc469f40f10b2283b
MikiTran <mikitran721@gmail.com>
2024-03-13 00:52:46 +0700

    update progress bar when playing and editing the bar width

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const wrapper = document.querySelector(".wrapper"),
   playPauseBtn = wrapper.querySelector(".play-pause"),
   prevBtn = wrapper.querySelector("#prev"),
   nextBtn = wrapper.querySelector("#next"),
+  progressArea = wrapper.querySelector(".progress-area"),
   progressBar = wrapper.querySelector(".progress-bar"),
   mainAudio = wrapper.querySelector("#main-audio");
 
@@ -75,4 +76,38 @@ mainAudio.addEventListener("timeupdate", (e) => {
   const duration = e.target.duration;
   let progressWidth = (currentTime / duration) * 100;
   progressBar.style.width = `${progressWidth}%`;
+
+  let musicCurrentTime = wrapper.querySelector(".current"),
+    musicDuration = wrapper.querySelector(".duration");
+
+  mainAudio.addEventListener("loadeddata", () => {
+    //update song total duration
+    let audioDuration = mainAudio.duration;
+    let totalMin = Math.floor(audioDuration / 60);
+    let totalSec = Math.floor(audioDuration % 60);
+    if (totalSec < 10) {
+      totalSec = `0${totalSec}`;
+    }
+    musicDuration.innerText = `${totalMin}:${totalSec}`;
+  });
+
+  //update playing song current time
+  let currentMin = Math.floor(currentTime / 60);
+  let currentSec = Math.floor(currentTime % 60);
+  if (currentSec < 10) {
+    currentSec = `0${currentSec}`;
+  }
+  musicCurrentTime.innerText = `${currentSec}:${currentMin}`;
 });
+
+// update playing song current time according to the progress bar width
+progressArea.addEventListener("click", (e) => {
+  let progressWidthVal = progressArea.clientWidth;
+  let clickedOffsetX = e.offsetX; //getting offset x value
+  let songDuration = mainAudio.duration;
+
+  mainAudio.currentTime = (clickedOffsetX / progressWidthVal) * songDuration;
+  playMusic();
+});
+
+// Work with: repeat, shuffle song according to the icon
