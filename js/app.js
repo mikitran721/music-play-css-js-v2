25d70b0d8dd77a735052724a06727718f9d9c601
MikiTran <mikitran721@gmail.com>
2024-03-12 21:41:23 +0700

    js-p1: init & play-pause song

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//select all required tags or elements
+const wrapper = document.querySelector(".wrapper"),
+  musicImg = wrapper.querySelector(".img-area img"),
+  musicName = wrapper.querySelector(".song-details .name"),
+  musicArtist = wrapper.querySelector(".song-details .artist"),
+  playPauseBtn = wrapper.querySelector(".play-pause"),
+  mainAudio = wrapper.querySelector("#main-audio");
+
+let musicIndex = 2;
+
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+});
+
+// load music function
+function loadMusic(indexNum) {
+  musicName.innerText = allMusic[indexNum - 1].name;
+  musicArtist.innerText = allMusic[indexNum - 1].artist;
+  musicImg.src = `images/${allMusic[indexNum - 1].img}.jpg`;
+  mainAudio.src = `songs/${allMusic[indexNum - 1].src}.m4a`;
+}
+
+// play music function
+function playMusic() {
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+// pause music function
+function pauseMusic() {
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+// play or pause music button event
+playPauseBtn.addEventListener("click", () => {
+  const isMusicPaused = wrapper.classList.contains("paused");
+  isMusicPaused ? pauseMusic() : playMusic();
+});
---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Bảo đỉnh chi đỉnh",
+    artist: "Li Yugang",
+    img: "music-1",
+    src: "2018.BaoDinhChiDinh",
+  },
+  {
+    name: "Bồ đề",
+    artist: "Li Yugang",
+    img: "music-2",
+    src: "2018.BoDe",
+  },
+  {
+    name: "Hoa Khôi",
+    artist: "Li Yugang",
+    img: "music-3",
+    src: "2018.HoaKhoi",
+  },
+  {
+    name: "Mạch Thượng Phong Nhã",
+    artist: "Li Yugang",
+    img: "music-4",
+    src: "2018.MachThuongPhongNha",
+  },
+  {
+    name: "Vầng trăng cong cong",
+    artist: "Li Yugang",
+    img: "music-5",
+    src: "2018.VangTrangCongCong",
+  },
+  {
+    name: "Trường Xuân",
+    artist: "Li Yugang",
+    img: "music-6",
+    src: "2019.TruongXuan",
+  },
+];
